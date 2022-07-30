37bc4bcf9530783602094385673cf8670db948ad
Az1yaT <jeenbek0vv.06@gmail.com>
2022-07-30 10:38:57 +0600

    HomeWork

---
+++ Task 47/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+Console.Write("Введите число m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] arr = new int[m,n];
+Random rnd = new Random();
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        arr[i,j] = rnd.Next(1,100);
+        Console.Write("{0,4}", arr[i, j]);
+    }
+    Console.WriteLine();
+}
---
+++ Task 50/Program.cs
@@ -0,0 +1,72 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого числа в массиве нет
+
+Console.Clear();
+int[,] FillMatrixRndInt(int m, int n)
+{
+    int[,] array = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0)
+            {
+                Console.Write("[");
+            }
+            if (j < array.GetLength(1) - 1)
+            {
+                Console.Write(array[i, j] + ",");
+            }
+            else
+            {
+                Console.Write(array[i, j] + "]");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindElemThroughtPos(int[,] array)
+{
+    Console.WriteLine($"Поажлуйста,введите номер позиции элемента для матрицы {array.GetLength(0)} x {array.GetLength(1)}:");
+    Console.Write("Введите номер строки элемента:");
+    int indexRow = Convert.ToInt32(Console.ReadLine()) - 1;
+    Console.Write("Введите номер столбца элемента:");
+    int indexCol = Convert.ToInt32(Console.ReadLine()) - 1;
+
+    while (indexRow >= array.GetLength(0) || indexCol >= array.GetLength(1))
+    {
+        Console.WriteLine($"Введенные значения {indexRow + 1} и {indexCol + 1} выходят за границы. Введите еще раз.");
+        Console.Write("Введите номер столбца эелмента:");
+        indexRow = Convert.ToInt32(Console.ReadLine())-1;
+        Console.Write("Введите номер столбца элемента:");
+        indexCol = Convert.ToInt32(Console.ReadLine())-1;
+    }
+    Console.Write($"Такой элемент есть: {array[indexRow, indexCol]}");
+}
+Console.Write($"Пожалуйста, введите число строк матрицы: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Пожалуйста, введите число столбцов матрицы: ");
+int col = Convert.ToInt32(Console.ReadLine());
+int[,] arrayRes = FillMatrixRndInt(row, col);
+PrintMatrix(arrayRes);
+FindElemThroughtPos(arrayRes);
